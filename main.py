5c133c7a048cdd3e67d8b37f76f4bc76f9928260
Espeon196 <z3ijnn3@gmail.com>
2025-10-01 00:56:04 +0900

    quickstart実行

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from prefect-quickstart!")
+from prefect import flow, task
+import random
 
 
-if __name__ == "__main__":
-    main()
+@task
+def get_customer_ids() -> list[str]:
+    # Fetch customer IDs from a database or API
+    return [f"customer-{n}" for n in random.choices(range(100), k=10)]
+
+
+@task
+def process_customer(customer_id: str) -> str:
+    # Process a single customer
+    return f"Processed {customer_id}"
+
+
+@flow
+def main() -> list[str]:
+    customer_ids = get_customer_ids()
+    # Map the process_customer task across all customer IDs
+    results = process_customer.map(customer_ids)
+    return results
+
+
+if __name__ == '__main__':
+    main.serve(name="my-first-deployment", cron="55 * * * *")
